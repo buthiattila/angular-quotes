568c397c41f4300b4fea518151aaea92a6fa6f68
buthiattila <buthi.attila@gmail.com>
2023-05-25 18:58:29 +0200

    elso commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, Title, Meta} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from "./features/public/header/header.component";
+
+import {AppRoutingModule} from './app-routing.module';
+import {MaterialModule} from './utils/material.module';
+import {HomeComponent} from "./features/public/home/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    MaterialModule,
   ],
-  providers: [],
+  exports: [],
+  providers: [
+    Title,
+    Meta,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
